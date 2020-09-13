c5c4b89098911edbcde2448bb63170dd6835bcc5
zhulu <shaonianzhentan@sina.cn>
2020-09-13 16:42:45 +0800

    随机播放叮咚

--- robot/player.py
+++ robot/player.py
@@ -1,5 +1,5 @@
 # -*- coding:UTF-8 -*-
-import pyaudio, os
+import pyaudio, os, random
 import wave
 import time
 import subprocess
@@ -11,46 +11,16 @@ class Player():
         pass
 
     def play_ding(self):
-        cmd = self.config.get('ding', '')
-        # 如果ding的命令不为空，则执行命令
-        if cmd != '':
-            os.system(cmd)
-            return
-        ding_wav = wave.open("snowboy/resources/ding.wav", 'rb')
-        ding_data = ding_wav.readframes(ding_wav.getnframes())
-        audio = pyaudio.PyAudio()
-        stream_out = audio.open(
-            format=audio.get_format_from_width(ding_wav.getsampwidth()),
-            channels=ding_wav.getnchannels(),
-            rate=ding_wav.getframerate(), input=False, output=True)
-        stream_out.start_stream()
-        stream_out.write(ding_data)
-        time.sleep(0.2)
-        stream_out.stop_stream()
-        stream_out.close()
-        audio.terminate()
+        mp3_path = 'ding'
+        file_list = os.listdir(mp3_path)
+        index = random.randint(0, len(file_list) - 1)
+        self.play_music(mp3_path + '/' + file_list[index])
 
     def play_dong(self):
-        cmd = self.config.get('dong', '')
-        # 如果ding的命令不为空，则执行命令
-        if cmd != '':
-            os.system(cmd)
-            return
-        ding_wav = wave.open("snowboy/resources/dong.wav", 'rb')
-        ding_data = ding_wav.readframes(ding_wav.getnframes())
-        audio = pyaudio.PyAudio()
-        stream_out = audio.open(
-            format=audio.get_format_from_width(ding_wav.getsampwidth()),
-            channels=ding_wav.getnchannels(),
-            rate=ding_wav.getframerate(), input=False, output=True)
-        stream_out.start_stream()
-        stream_out.write(ding_data)
-        time.sleep(0.2)
-        stream_out.stop_stream()
-        stream_out.close()
-        audio.terminate()
+        mp3_path = 'dong'
+        file_list = os.listdir(mp3_path)
+        index = random.randint(0, len(file_list) - 1)
+        self.play_music(mp3_path + '/' + file_list[index])
 
     def play_music(self, fname):
-        subprocess.Popen(['mpg123', '-q', fname]).wait()
-        
-# Player().play_music("http://music.163.com/song/media/outer/url?id=108485.mp3")
+        subprocess.Popen(['mpg123', '-a', 'hw:0,0', '-q', fname]).wait()
